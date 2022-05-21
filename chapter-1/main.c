fc32eabb7374830fd7561f8b1a5e14d8269959d5
Andrew <andrewnijmeh1@gmail.com>
2022-05-21 13:37:08 -0400

    arrays

--- chapter-1/main.c
+++ chapter-1/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
- printf("hello world! andrew here");
+ printf("hello world! andrew here\n");
  return 0;
 }
---
+++ chapter-4/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int age[] = {13, 14, 16};
+  
+  printf("%d\n", age[0]);
+  printf("%d\n", age[1]);
+  printf("%d\n", age[2]);
+  return 0;
+
+}
